87845f60bca1a24aafe70b9d7b31e6209e91b919
Paul Braham <paul.braham@disclosurescotland.scot.uk>
2023-03-09 13:56:10 +0000

    updated script to take input from Terraform

--- github_cidrs.py
+++ github_cidrs.py
@@ -5,10 +5,15 @@ import requests
 import sys
 
 # Edit the following section to change script constants
-CIDR_BLOCKS_REQUIRED = ["web","api"]
 FILTER_IPV6 = True
 GITHUB_API_URL = "https://api.github.com/meta"
 TIMEOUT = 5
+
+#Get query from Terraform
+terraform_input = sys.stdin.read()
+terraform_input_json = json.loads(terraform_input)
+
+cidr_blocks_required = terraform_input_json.get("cidr_blocks_required")
 
 # Try to get response from Github API
 try:
@@ -25,7 +30,7 @@ except requests.exceptions.HTTPError as e:
     raise SystemExit(1) from e
 
 cidr_list = []
-for i in CIDR_BLOCKS_REQUIRED:
+for i in cidr_blocks_required:
     if i in github_response.json():
         for x in github_response.json()[i]:
             if FILTER_IPV6:
